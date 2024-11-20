cff19358e8aa79e5b0ed878d98e79ca5c5c3d783
baamadu <baamadu112@gmail.com>
2024-11-20 10:06:01 +0100

    sprint 1 en 0 af

--- BKE/main.js
+++ BKE/main.js
@@ -1,1 +1,11 @@
 console.log("Main loaded");
+// Haal alle vakken op met querySelectorAll
+const vakjes = document.querySelectorAll(".vakje");
+console.log(vakjes);
+
+for (let i = 0; i < vakjes.length; i++) {
+  const vakje = vakjes[i];
+  vakje.addEventListener("click", function () {
+    vakje.textContent = "O";
+  });
+}
