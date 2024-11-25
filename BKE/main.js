4aa274ab5ce887a85bf8ca2d5e9b5238ad4a36a1
baamadu <baamadu112@gmail.com>
2024-11-25 12:58:11 +0100

    Toon een X of een O op het speelbord als de speler erop klikt, steeds om en om

--- BKE/main.js
+++ BKE/main.js
@@ -2,10 +2,24 @@ console.log("Main loaded");
 // Haal alle vakken op met querySelectorAll
 const vakjes = document.querySelectorAll(".vakje");
 console.log(vakjes);
-
+ 
+//for (let i = 0; i < vakjes.length; i++) {
+ // const vakje = vakjes[i];
+  //vakje.addEventListener("click", function () {
+    //vakje.textContent = "O";
+  //});
+//}
+let currentPlayer = 'X'
 for (let i = 0; i < vakjes.length; i++) {
   const vakje = vakjes[i];
-  vakje.addEventListener("click", function () {
-    vakje.textContent = "O";
+  vakje.addEventListener('click', function () {
+    // Als het vakje nog leeg is, voeg de zet van de huidige speler toe
+    if (vakje.textContent === '') {
+      vakje.textContent = currentPlayer;
+ 
+      // Wissel de beurt na elke zet
+      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    }
   });
 }
+
