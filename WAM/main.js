bccca95a5c0bbc99f1fbc98f7c8c0bc3942eb897
Calvin752 <raijinsan2@gmail.com>
2024-11-27 09:43:51 +0100

    code over dat er gecontroleerd word of er een mol op het veld staat

--- WAM/main.js
+++ WAM/main.js
@@ -3,3 +3,33 @@ const circles = document.querySelectorAll('.circle')
 circles.forEach((circle, i) => {
     console.log(`Circle ${i + 1}:`, circle);
 });
+
+circles.forEach(function(circle) {
+    circle.addEventListener('click', function() {
+        if (circle.classList.contains('filled')) {
+            console.log('Deze cirkel is al gevuld');
+        }
+        
+        if (circle.classList.contains('mole')) {
+            console.log('Je hebt de mol geraakt');
+        } else {
+            console.log('Geen mol in dit veld');
+        }
+
+        circle.classList.add('filled');
+
+        if (circle.classList.contains('mole')) {
+            console.log('Mol werd geraakt');
+        }
+        console.log('Veld gevuld!');
+    });
+});
+
+
+
+
+
+
+
+
+
