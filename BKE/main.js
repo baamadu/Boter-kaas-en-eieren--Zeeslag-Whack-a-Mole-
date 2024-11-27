4010691be3c29aff76e53d1d8089143b907f6972
baamadu <baamadu112@gmail.com>
2024-11-27 10:05:17 +0100

    - Maak de layout voor een X of een O op het speelbord - Maak de eventListener op alle velden van het speelbord - Toon een X of een O op het speelbord als de speler erop klikt, steeds om en om - Zorg ervoor dat als een veld al gevuld is, dat er geen andere invoer mogelijk is

--- BKE/main.js
+++ BKE/main.js
@@ -2,24 +2,20 @@ console.log("Main loaded");
 // Haal alle vakken op met querySelectorAll
 const vakjes = document.querySelectorAll(".vakje");
 console.log(vakjes);
- 
+
 //for (let i = 0; i < vakjes.length; i++) {
- // const vakje = vakjes[i];
-  //vakje.addEventListener("click", function () {
-    //vakje.textContent = "O";
-  //});
+// const vakje = vakjes[i];
+//vakje.addEventListener("click", function () {
+//vakje.textContent = "O";
+//});
 //}
-let currentPlayer = 'X'
+let currentPlayer = "X";
 for (let i = 0; i < vakjes.length; i++) {
   const vakje = vakjes[i];
-  vakje.addEventListener('click', function () {
-    // Als het vakje nog leeg is, voeg de zet van de huidige speler toe
-    if (vakje.textContent === '') {
+  vakje.addEventListener("click", function () {
+    if (vakje.textContent === "") {
       vakje.textContent = currentPlayer;
- 
-      // Wissel de beurt na elke zet
-      currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+      currentPlayer = currentPlayer === "X" ? "O" : "X";
     }
   });
 }
-
