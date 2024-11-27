5183c9f83c60ad261ece339640a90b374f407923
baamadu <baamadu112@gmail.com>
2024-11-27 10:09:57 +0100

    Schrijf een berekening/ manier om alle velden te controleren op drie-op-een-rij

--- BKE/main.js
+++ BKE/main.js
@@ -3,13 +3,7 @@ console.log("Main loaded");
 const vakjes = document.querySelectorAll(".vakje");
 console.log(vakjes);
 
-//for (let i = 0; i < vakjes.length; i++) {
-// const vakje = vakjes[i];
-//vakje.addEventListener("click", function () {
-//vakje.textContent = "O";
-//});
-//}
-let currentPlayer = "X";
+ currentPlayer = "X";
 for (let i = 0; i < vakjes.length; i++) {
   const vakje = vakjes[i];
   vakje.addEventListener("click", function () {
@@ -19,3 +13,17 @@ for (let i = 0; i < vakjes.length; i++) {
     }
   });
 }
+
+//De mogelijke winnende combinaties:
+
+//Horizontaal:
+//Rij 1: vakje 1, vakje 2, vakje 3
+//Rij 2: vakje 4, vakje 5, vakje 6
+//Rij 3: vakje 7, vakje 8, vakje 9
+//Verticaal:
+//Kolom 1: vakje 1, vakje 4, vakje 7
+//Kolom 2: vakje 2, vakje 5, vakje 8
+//Kolom 3: vakje 3, vakje 6, vakje 9
+//Diagonalen:
+//Diagonaal 1: vakje 1, vakje 5, vakje 9
+//Diagonaal 2: vakje 3, vakje 5, vakje 7
